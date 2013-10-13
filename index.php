9b0d96b0ea5aecf7544a67f16cfb905b49710259
Gabriel Izaias <gabriel.izaias@gmail.com>
2013-10-13 13:50:52 -0300

    Adiciona nova estrutura CSS (Sass)

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Taz Manager</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body role="application">
+    <header id="header" role="banner">
+
+    </header>
+    <div id="main" role="main">
+
+    </div>
+</body>
+</html>
