653bf8f7bfeb4eca061043f10e031e3f02078ce2
Gabriel Izaias <gabriel.izaias@gmail.com>
2013-10-15 18:49:08 -0300

    Adiciona layout da navegação

--- index.php
+++ index.php
@@ -6,11 +6,26 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body role="application">
-    <header id="header" role="banner">
-
+    <header id="nav" role="banner">
+        <i class="logo icon icon-logo icon-logo-s">Taz Manager</i>
+        <nav class="nav">
+            <div class="nav-box">
+                <h4 class="nav-title">Dashboard</h4>
+                <ul class="nav-list">
+                    <li class="nav-item selected"><a href="#" class="nav-link"><i class="glyphicon glyphicon-dashboard"></i> Dashboard</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link"><i class="glyphicon glyphicon-inbox"></i> Minhas tarefas</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link"><i class="glyphicon glyphicon-hand-right"></i> Tarefas delegadas</a></li>
+                </ul>
+            </div>
+            <div class="nav-box">
+                <h4 class="nav-title">Projetos</h4>
+                <ul class="nav-list">
+                    <li class="nav-item"><a href="#" class="nav-link"><i class="glyphicon glyphicon-folder-close"></i> Projeto X</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link"><i class="glyphicon glyphicon-folder-close"></i> Novo site</a></li>
+                </ul>
+            </div>
+        </nav>
     </header>
-    <div id="main" role="main">
-
-    </div>
+    <div id="main" role="main"></div>
 </body>
 </html>
