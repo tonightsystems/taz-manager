20fe268b8bcafdee673372351277c34bd7c21956
Gabriel Izaias <gabriel.izaias@gmail.com>
2013-12-04 00:40:23 -0200

    Adiciona dependencia do concurrent

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,6 +27,29 @@ module.exports = function(grunt) {
                 jshintrc: true
             }
         },
+        concurrent: {
+            tasks: ['nodemon', 'watch'],
+            options: {
+                logConcurrentOutput: true
+            }
+        },
+        nodemon: {
+            dev: {
+                options: {
+                    file: 'app.js',
+                    args: [],
+                    ignoredFiles: ['README.md', 'node_modules/**', '.DS_Store'],
+                    watchedExtensions: ['js'],
+                    watchedFolders: ['app', 'config'],
+                    debug: true,
+                    delayTime: 1,
+                    env: {
+                        PORT: 8080
+                    },
+                    cwd: __dirname
+                }
+            }
+        },
         uglify: {
             dist: {
                 src: ['js/**/*.js'],
@@ -50,6 +73,8 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-nodemon');
+    grunt.loadNpmTasks('grunt-concurrent');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('default', ['concurrent']);
 }
